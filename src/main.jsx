3ceee9fb55ec49b75a1fbb5278a3e737ca203601
nexapath <alex.t@nexapath.us>
2025-08-11 18:43:55 -0700

    080825Final

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,5 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
 import './index.css'
-
-createRoot(document.getElementById('root')).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-)
+import App from './App.jsx'
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>)
