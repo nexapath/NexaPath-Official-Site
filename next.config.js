6e084fe86a04f6b99e71dffbdb2df37aef94442c
nexapath <alex.t@nexapath.us>
2025-08-11 21:49:28 -0700

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  i18n: {
-    locales: ['en', 'zh-Hant'],
-    defaultLocale: 'en'
-  },
-  images: { unoptimized: true }
-};
-module.exports = nextConfig;
